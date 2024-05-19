bd9623f861f3aa9b33ab9562b845571d1ed7e965
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-05-19 18:39:21 +0900

    feat: AudioWorkletProcessor

---
+++ docs/audio-worklets/a-rate.js
@@ -0,0 +1,35 @@
+class NoiseGeneratorProcessor extends AudioWorkletProcessor {
+  static get parameterDescriptors() {
+    return [
+      {
+        name: 'noiseGain',
+        defaultValue: 1,
+        minValue: 0,
+        maxValue: 1,
+        automationRate: 'a-rate'
+      }
+    ];
+  }
+
+  constructor() {
+    super();
+  }
+
+  process(inputs, outputs, parameters) {
+    const output = outputs[0];
+
+    const gain = parameters.noiseGain;
+
+    for (let channelNumber = 0, numberOfChannels = output.length; channelNumber < numberOfChannels; channelNumber++) {
+      const bufferSize = output[channelNumber].length;
+
+      for (let n = 0; n < bufferSize; n++) {
+        output[channelNumber][n] = (gain.length > 1 ? gain[n] : gain[0]) * (2 * Math.random() - 1);
+      }
+    }
+
+    return true;
+  }
+}
+
+registerProcessor('NoiseGeneratorProcessor', NoiseGeneratorProcessor);
