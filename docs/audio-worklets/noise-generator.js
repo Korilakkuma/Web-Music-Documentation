54eee164c83f7d856a750c538dd2e0f81f4dd354
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-06-01 22:00:06 +0900

    feat: AudioWorklet examples

---
+++ docs/audio-worklets/noise-generator.js
@@ -0,0 +1,81 @@
+class NoiseGeneratorProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+
+    this.type = 'whitenoise';
+
+    this.b0 = 0;
+    this.b1 = 0;
+    this.b2 = 0;
+    this.b3 = 0;
+    this.b4 = 0;
+    this.b5 = 0;
+    this.b6 = 0;
+
+    this.lastOut = 0;
+
+    this.port.onmessage = (event) => {
+      if (event.data.type) {
+        this.type = event.data.type;
+      }
+    };
+  }
+
+  process(inputs, outputs, parameters) {
+    const output = outputs[0];
+
+    switch (this.type) {
+      case 'whitenoise': {
+        for (let channelNumber = 0, numberOfChannels = output.length; channelNumber < numberOfChannels; channelNumber++) {
+          for (let n = 0; n < 128; n++) {
+            output[channelNumber][n] = 2 * Math.random() - 1;
+          }
+        }
+
+        break;
+      }
+
+      case 'pinknoise': {
+        for (let channelNumber = 0, numberOfChannels = output.length; channelNumber < numberOfChannels; channelNumber++) {
+          for (let n = 0; n < 128; n++) {
+            const white = 2 * Math.random() - 1;
+
+            this.b0 = 0.99886 * this.b0 + white * 0.0555179;
+            this.b1 = 0.99332 * this.b1 + white * 0.0750759;
+            this.b2 = 0.969 * this.b2 + white * 0.153852;
+            this.b3 = 0.8665 * this.b3 + white * 0.3104856;
+            this.b4 = 0.55 * this.b4 + white * 0.5329522;
+            this.b5 = -0.7616 * this.b5 - white * 0.016898;
+
+            output[channelNumber][n] = this.b0 + this.b1 + this.b2 + this.b3 + this.b4 + this.b5 + this.b6 + white * 0.5362;
+            output[channelNumber][n] *= 0.11;
+
+            this.b6 = white * 0.115926;
+          }
+        }
+
+        break;
+      }
+
+      case 'browniannoise': {
+        for (let channelNumber = 0, numberOfChannels = output.length; channelNumber < numberOfChannels; channelNumber++) {
+          for (let n = 0; n < 128; n++) {
+            const white = 2 * Math.random() - 1;
+
+            output[n] = (this.lastOut + 0.02 * white) / 1.02;
+
+            this.lastOut = (this.lastOut + 0.02 * white) / 1.02;
+
+            outputs[n] *= 3.5;
+          }
+        }
+
+        break;
+      }
+    }
+
+    return true;
+  }
+}
+
+registerProcessor('NoiseGeneratorProcessor', NoiseGeneratorProcessor);
