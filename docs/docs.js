feb27a27e845e3b741b23fd83b69bfd98ade966c
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-11-20 18:00:04 +0900

    feat: High-Pass Filter (BiquadFilterNode)

--- docs/docs.js
+++ docs/docs.js
@@ -7532,3 +7532,4 @@ ringmodulator();
 animateAM(document.getElementById('svg-animation-amplitude-modulation-time'), document.getElementById('svg-animation-amplitude-modulation-spectrum'));
 
 renderFrequencyResponse(document.getElementById('svg-figure-filter-response-lowpass'), 'lowpass');
+renderFrequencyResponse(document.getElementById('svg-figure-filter-response-highpass'), 'highpass');
