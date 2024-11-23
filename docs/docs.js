98baea4fa16d3aa558d192a99f2812f8cee059cb
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-11-23 20:00:21 +0900

    feat: Band-Pass Filter (BiquadFilterNode)

--- docs/docs.js
+++ docs/docs.js
@@ -7533,3 +7533,4 @@ animateAM(document.getElementById('svg-animation-amplitude-modulation-time'), do
 
 renderFrequencyResponse(document.getElementById('svg-figure-filter-response-lowpass'), 'lowpass');
 renderFrequencyResponse(document.getElementById('svg-figure-filter-response-highpass'), 'highpass');
+renderFrequencyResponse(document.getElementById('svg-figure-filter-response-bandpass'), 'bandpass');
