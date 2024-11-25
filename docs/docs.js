0702cc64d3a75a67fd89e81966868e37e1d23e4e
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-11-25 20:00:01 +0900

    feat: Low-Shelving Filter (BiquadFilterNode)

--- docs/docs.js
+++ docs/docs.js
@@ -7534,3 +7534,4 @@ animateAM(document.getElementById('svg-animation-amplitude-modulation-time'), do
 renderFrequencyResponse(document.getElementById('svg-figure-filter-response-lowpass'), 'lowpass');
 renderFrequencyResponse(document.getElementById('svg-figure-filter-response-highpass'), 'highpass');
 renderFrequencyResponse(document.getElementById('svg-figure-filter-response-bandpass'), 'bandpass');
+renderFrequencyResponse(document.getElementById('svg-figure-filter-response-lowshelf'), 'lowshelf');
